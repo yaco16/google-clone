30e96254b02870ead31567e5a256892bdeace88c
yaco16 <65079827+yaco16@users.noreply.github.com>
2021-05-31 17:01:23 +0200

    add google logo and search bar

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from 'next/head';
+import Image from 'next/image';
 import Avatar from '../components/Avatar';
 import { MicrophoneIcon, ViewGridIcon } from "@heroicons/react/solid";
+import { SearchIcon } from "@heroicons/react/outline";
 export default function Home() {
   return (
     <div>
@@ -31,6 +33,19 @@ export default function Home() {
       </header>
 
       {/* Body */}
+      <form>
+        <Image
+          src="https://www.google.co.uk/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
+          height={100}
+          width={300}
+        />
+
+        <div className="flex w-full mt-5 hover:shadow-lg focus-within:shadow-lg max-w-md rounded-full border border-gray-200 px-5 py-3 items-center sm:max-w-xl lg:max-w-2xl" >
+          <SearchIcon className=" h-5 mr-3 text-gray-500" />
+          <input type="text" className="flex-grow focus:outline-none" />
+          <MicrophoneIcon className=" h-5 mr-3 text-gray-500"/>
+        </div>
+      </form>
 
       {/* Footer */}
 
