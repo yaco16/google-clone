2a6e744823c3da677a9c2b86f32de582fdca52ba
yaco16 <65079827+yaco16@users.noreply.github.com>
2021-06-03 16:13:24 +0200

    prevent form to reload

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,16 @@ import Avatar from '../components/Avatar';
 import { MicrophoneIcon, ViewGridIcon } from "@heroicons/react/solid";
 import { SearchIcon } from "@heroicons/react/outline";
 import Footer from '../components/Footer';
+import { useRef } from 'react';
+
 export default function Home() {
+  const searchInputRef = useRef(null);
+
+  const search = (e) => {
+    e.preventDefault();
+
+  }
+
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <Head>
@@ -43,14 +52,15 @@ export default function Home() {
 
         <div className="flex w-full mt-5 hover:shadow-lg focus-within:shadow-lg max-w-md rounded-full border border-gray-200 px-5 py-3 items-center sm:max-w-xl lg:max-w-2xl" >
           <SearchIcon className=" h-5 mr-3 text-gray-500" />
-          <input type="text" className="flex-grow focus:outline-none" />
+          <input
+          ref = {searchInputRef}
+          type="text" className="flex-grow focus:outline-none" />
           <MicrophoneIcon className=" h-5 mr-3 text-gray-500"/>
         </div>
 
       <div className="flex flex-col w-1/2 space-y-2 justify-center mt-8 sm:space-y-0 sm:flex-row sm:space-x-4">
-        <button className="button">Google search</button>
-
-        <button className="button">I'm feeling lucky</button>
+        <button onClick={search} className="button">Google search</button>
+        <button onClick={search} className="button">I'm feeling lucky</button>
       </div>
       </form>
 
