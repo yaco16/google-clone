11ffa16b7e7667532c872d040bb741324ee6aef3
yaco16 <65079827+yaco16@users.noreply.github.com>
2021-06-03 16:21:16 +0200

    create search page

---
+++ pages/search.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+function Search() {
+  return (
+    <div>
+    <Head>
+      <title>Search results</title>
+    </Head>
+
+    </div>
+  )
+}
+
+export default Search;
