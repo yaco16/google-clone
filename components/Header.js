e6063304d571d1366a9585bd164f89834e9d635e
yaco16 <65079827+yaco16@users.noreply.github.com>
2021-06-03 17:26:42 +0200

    upd Header add erase cross in input text when cross is pressed, the text is erased

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,11 @@
+import { MicrophoneIcon, SearchIcon, XIcon } from '@heroicons/react/solid';
 import Image from 'next/image';
-import { useRouter } from "next/router";
+import { useRouter } from 'next/router';
+import { useRef } from 'react';
 
 function Header() {
   const router = useRouter();
+  const searchInputRef = useRef(null);
 
   return (
     <header>
@@ -10,11 +13,23 @@ function Header() {
         src='https://www.google.co.uk/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png'
         height={40}
         width={120}
-        className='cursor-pointer'
         onClick={() => router.push('/')}
+        className='cursor-pointer'
+      />
+
+      <form className='flex px-6 py-3 ml-10 mr-5 border border-gray-200 rounded-full shadow-lg max-w-3xl items-center'>
+        <input
+          ref={searchInputRef}
+          className='flex-grow w-full focus:outline-none'
+          type='text'
         />
+        <XIcon
+          className='h-10 sm:mr-3 text-gray-500 cursor-pointer transition duration-100 transform hover:scale-125'
+          onClick={() => (searchInputRef.current.value = '')}
+        />
+      </form>
     </header>
-  )
+  );
 }
 
 export default Header;
