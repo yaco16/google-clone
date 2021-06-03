8c5b89d9b71d442d2faae0d41decfa4eb1a5ed98
yaco16 <65079827+yaco16@users.noreply.github.com>
2021-06-03 16:24:53 +0200

    create Header for results page

--- pages/search.js
+++ pages/search.js
@@ -1,11 +1,18 @@
 import Head from "next/head";
+import Header from "../components/Header";
 
 function Search() {
   return (
     <div>
     <Head>
       <title>Search results</title>
+      <link rel="icon" href="/favicon.ico" />
     </Head>
+
+    {/* Header */}
+    <Header />
+
+    {/* Sarch results */}
 
     </div>
   )
