42902bf04f1738b679bda355803c5156605f14f2
yaco16 <65079827+yaco16@users.noreply.github.com>
2021-06-03 16:31:21 +0200

    uppd Header

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,19 @@
-function Header() {
-  return (
-    <div>
+import Image from 'next/image';
+import { useRouter } from "next/router";
 
-    </div>
+function Header() {
+  const router = useRouter();
+
+  return (
+    <header>
+      <Image
+        src='https://www.google.co.uk/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png'
+        height={40}
+        width={120}
+        className='cursor-pointer'
+        onClick={() => router.push('/')}
+        />
+    </header>
   )
 }
 
